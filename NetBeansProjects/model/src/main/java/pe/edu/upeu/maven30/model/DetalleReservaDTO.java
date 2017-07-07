ff314b8d3757b3c7390a7e82b92f73fcbcc960b6
junio <junio@DESKTOP-C6CKLDR>
2017-07-07 04:24:17 -0500

    KEVIN JUNIOR - detalledto

--- NetBeansProjects/model/src/main/java/pe/edu/upeu/maven30/model/DetalleReservaDTO.java
+++ NetBeansProjects/model/src/main/java/pe/edu/upeu/maven30/model/DetalleReservaDTO.java
@@ -10,5 +10,57 @@ package pe.edu.upeu.maven30.model;
  * @author junio
  */
 public class DetalleReservaDTO {
+    private String dre_descripcion;
+    private String dre_estado;
+    private int re_id;
+    private int eq_id;
+
+    public DetalleReservaDTO() {
+    }
+
+    
+    public DetalleReservaDTO(String dre_descripcion, String dre_estado) {
+        this.dre_descripcion = dre_descripcion;
+        this.dre_estado = dre_estado;
+    }
+
+    public DetalleReservaDTO(String dre_descripcion, String dre_estado, int re_id, int eq_id) {
+        this.dre_descripcion = dre_descripcion;
+        this.dre_estado = dre_estado;
+        this.re_id = re_id;
+        this.eq_id = eq_id;
+    }
+
+    public String getDre_descripcion() {
+        return dre_descripcion;
+    }
+
+    public void setDre_descripcion(String dre_descripcion) {
+        this.dre_descripcion = dre_descripcion;
+    }
+
+    public String getDre_estado() {
+        return dre_estado;
+    }
+
+    public void setDre_estado(String dre_estado) {
+        this.dre_estado = dre_estado;
+    }
+
+    public int getRe_id() {
+        return re_id;
+    }
+
+    public void setRe_id(int re_id) {
+        this.re_id = re_id;
+    }
+
+    public int getEq_id() {
+        return eq_id;
+    }
+
+    public void setEq_id(int eq_id) {
+        this.eq_id = eq_id;
+    }
     
 }
